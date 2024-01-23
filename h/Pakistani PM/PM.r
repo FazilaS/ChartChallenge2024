73a3f2d7bde3cdacae53d4c802817d3b19a24faa
FazilaS <105195837+FazilaS@users.noreply.github.com>
2024-01-23 18:57:28 +0500

    Create PM.r

---
+++ h/Pakistani PM/PM.r
@@ -0,0 +1,40 @@
+library(ggthemes)
+library(ggimage)
+library(dplyr)
+
+data <- read.csv("pmlist.csv")
+view(data)
+df<- data %>% filter(Elections >= 2008) %>% 
+  select(Name, Time.days.)
+
+View(df)
+df<- df %>%
+  rename(tenure = Time.days.)
+### data edit ##########
+df[6,2] = 490
+
+df$image_file<-paste0('/cloud/project/', df$Name, '.png')
+
+ggplot(df,aes(tenure, Name , width = 0.782))+
+  geom_col(fill = '#003366')+
+  labs(title = 'Tenure of Prime Ministers in Pakistan, Since 2008',
+       caption = "Graphic@Fazila Sadia")+
+  geom_text(aes(label=paste(tenure, 'DAYS')), hjust = 1.2, color= 'white', 
+            size= 6.8, fontface  = 'bold.italic')+
+  geom_image(x =-65 , aes(image = image_file, ), size = 0.25)+
+  theme_void(base_size = 22)+
+  theme(
+    plot.background = element_rect(fill = "white"),
+    plot.title = element_text(family = "Palatino", face = 'bold', hjust= 0.09, size = 32),
+    plot.caption = element_text(family='times',
+                                face="plain",
+                                size=8,
+                                ),
+    plot.margin = unit(c(0.25, 0.15, 0.15, 0.28), 
+                       "inches")
+  )+ 
+  expand_limits(x = -30, y = 1) 
+
+
+
+  
